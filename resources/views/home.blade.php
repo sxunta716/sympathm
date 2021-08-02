1946137fb6116eba17c7c51722b24316a045588d
EC2 Default User <ec2-user@ip-172-31-30-53.ap-northeast-1.compute.internal>
2021-08-02 14:34:45 +0000

    ログイン機能を実装

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            おすすめリストは作成されていません
+            <a href="/artist/create" class="btn btn-primary">おすすめ作成</a>
+        </div>
+    </div>
+</div>
+@endsection
